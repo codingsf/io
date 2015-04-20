85342744e27a5d9e190e80ce5a292e7ad6607dca
reddec <net.dev@mail.ru>
2015-04-20 19:37:32 +0300

    Publisher changed to AsyncServer

--- src/experimental.cpp
+++ src/experimental.cpp
@@ -3,59 +3,21 @@
 //
 
 #include "experimental.h"
+#include <string>
 
 namespace io {
 
-    Publisher::Publisher(io::ConnectionManager::Ptr ptr) : manager_(ptr) {
-        if (ptr->has_valid_descriptor()) {
-            if (!epoll_.add(ptr->descriptor(), EPOLLIN, &Publisher::on_client, this))
-                set_error();
+    Publisher::Publisher(io::Epoll &epoll, io::ConnectionManager::Ptr serv_con_) : AsyncSocketServer(epoll,
+                                                                                                     serv_con_) { }
+
+    void Publisher::publish() {
+        std::string content = line_.str();
+        auto lock = lock_collection();
+        for (auto &kv:clients_) {
+            kv.second->output() << content;
+            kv.second->output().flush();
         }
-        if (ready()) {
-            thread_ = std::thread(std::bind(&Publisher::run, this));
-        }
-    }
-
-    Publisher::~Publisher() {
-        stop();
-        thread_.join();
-    }
-
-    void Publisher::clean() {
-        for (auto &kv:clients_) epoll_.remove(kv.first);
-        clients_.clear();
-        epoll_.remove(manager_->descriptor());
-    }
-
-    void Publisher::run() {
-        int count = 0;
-        while (count >= 0 && ready()) {
-            count = epoll_.poll(5000);
-        }
-    }
-
-    void Publisher::stop() {
-        clean();
-        stop_ = true;
-        epoll_.close();
-    }
-
-    void Publisher::on_client(io::Epoll &epoll, uint32_t event, int fd) {
-        std::unique_lock<std::mutex> lock(lock_);
-        if (event & EPOLLIN) {
-            int client = manager_->next_descriptor();
-            epoll.add(client, EPOLLHUP | EPOLLRDHUP | EPOLLERR, &Publisher::on_client_disconnect, this);
-            clients_[client] = io::FileStream::create(client);
-        } else {
-            //Error on accept - this is fatal
-            set_error();
-            stop();
-        }
-    }
-
-    void Publisher::on_client_disconnect(io::Epoll &epoll, uint32_t event, int fd) {
-        std::unique_lock<std::mutex> lock(lock_);
-        epoll.remove(fd);
-        clients_.erase(fd);
+        line_.str("");
+        line_.clear();
     }
 }
--- src/experimental.h
+++ src/experimental.h
@@ -7,54 +7,21 @@
 
 #include "io.h"
 #include "async.h"
-#include <mutex>
-#include <thread>
-
+#include <sstream>
 namespace io {
 
-    struct Publisher : public io::WithError {
-        Publisher(io::ConnectionManager::Ptr ptr);
 
-        template<class T>
-        void write_line(const T &obj) {
-            std::unique_lock<std::mutex> lock(lock_);
-            for (auto &kv: clients_) {
-                kv.second->output() << obj << std::endl;
-            }
-        }
+    struct Publisher : public io::AsyncSocketServer {
 
-        inline bool ready() const {
-            return !stop_ &&
-                   manager_->is_active() &&
-                   manager_->has_valid_descriptor() &&
-                   epoll_.has_valid_descriptor() &&
-                   !epoll_.has_error() &&
-                   !manager_->has_error();
-        }
+        Publisher(io::Epoll &epoll, io::ConnectionManager::Ptr serv_con_);
 
-        ~Publisher();
+        inline std::stringstream &line() { return line_; }
+
+        void publish();
 
     protected:
-
-        void clean(); //Remove clients and server from epoll
-
-        void run(); // Poll events
-
-        void stop(); // Clean and close epoll
-
-        void on_client(io::Epoll &epoll, uint32_t event, int fd);
-
-        void on_client_disconnect(io::Epoll &epoll, uint32_t event, int fd);
-
-
-    private:
-        bool stop_ = false;
-        std::thread thread_;
-        std::mutex lock_;
-        io::ConnectionManager::Ptr manager_;
-        io::Epoll epoll_;
-        std::unordered_map<int, io::FileStream::Ptr> clients_;
-        std::function<void()> idle_;
+        std::stringstream line_;
     };
+
 }
 #endif //IO_EXPERIMENTAL_H
